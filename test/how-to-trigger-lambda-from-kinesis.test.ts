7fa1039a823a11c0c68fccad8cc05b28727022d7
Andy <babyandy0111@gmail.com>
2022-10-03 14:17:19 +0800

    Initial commit

---
+++ test/how-to-trigger-lambda-from-kinesis.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HowToTriggerLambdaFromKinesis from '../lib/how-to-trigger-lambda-from-kinesis-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/how-to-trigger-lambda-from-kinesis-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HowToTriggerLambdaFromKinesis.HowToTriggerLambdaFromKinesisStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
